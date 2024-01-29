bde13667d604b3f0752d6a829c476a5d3f1690cf
localghost <155446689+localghost-mod@users.noreply.github.com>
2024-01-30 00:00:43 +0800

    Init

---
+++ Source/Alpha Prefabs - Blueprints/HarmonyPatches.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using AlphaPrefabs;
+using Blueprints;
+using HarmonyLib;
+using KCSG;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Alpha_Prefabs_Blueprints
+{
+    [StaticConstructorOnStartup]
+    public static class Startup
+    {
+        static Startup() => new Harmony("localghost.alpharefabsblueprint").PatchAll();
+    }
+
+    [HarmonyPatch(typeof(Window_Prefab), nameof(Window_Prefab.DoWindowContents))]
+    public static class HarmonyPatches
+    {
+        static void Postfix(Window_Prefab __instance, ref Rect inRect)
+        {
+            var outRect = new Rect(inRect);
+            outRect.yMin += 20f;
+            outRect.yMax -= 40f;
+            outRect.width -= 16f;
+            outRect.yMin += 20f;
+            Rect oderButtonRect = new Rect(
+                outRect.width / 2f + Window.CloseButSize.x,
+                outRect.height + 30,
+                Window.CloseButSize.x,
+                Window.CloseButSize.y
+            );
+            if (!__instance.prefab.variations.NullOrEmpty())
+                AlphaPrefabs.Utils.DrawButton(
+                    oderButtonRect,
+                    "AP_Export".Translate(),
+                    () =>
+                        Find.WindowStack.Add(
+                            new FloatMenu(
+                                __instance
+                                    .prefab.variations.Select(
+                                        variation =>
+                                            new FloatMenuOption(
+                                                variation.name.CapitalizeFirst(),
+                                                () =>
+                                                    __instance.Export(
+                                                        variation.layoutVariation,
+                                                        variation.name
+                                                    )
+                                            )
+                                    )
+                                    .Prepend(
+                                        new FloatMenuOption(
+                                            __instance.prefab.labelForDefaultVariation.CapitalizeFirst(),
+                                            () => __instance.Export()
+                                        )
+                                    )
+                                    .ToList()
+                            )
+                        )
+                );
+            else if (Widgets.ButtonText(oderButtonRect, "AP_ExportWithVariation".Translate()))
+                __instance.Export();
+        }
+
+        static void Export(
+            this Window_Prefab __instance,
+            StructureLayoutDef layoutVariation = null,
+            string variationString = null
+        ) =>
+            BlueprintController.Add(
+                Utils.ToBluePrint(
+                    layoutVariation ?? __instance.prefab.layout,
+                    $"{__instance.prefab.LabelCap}_{variationString ?? __instance.prefab.labelForDefaultVariation}"
+                )
+            );
+    }
+}
---
+++ Source/Alpha Prefabs - Blueprints/Utils.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using Blueprints;
+using HarmonyLib;
+using KCSG;
+using Verse;
+
+namespace Alpha_Prefabs_Blueprints
+{
+    public static class Utils
+    {
+        public static Blueprint ToBluePrint(this StructureLayoutDef structureLayoutDef, string name)
+        {
+            var layouts = Traverse
+                .Create(structureLayoutDef)
+                .Field("_layouts")
+                .GetValue<List<SymbolDef[,]>>();
+            var sizes = structureLayoutDef.Sizes;
+            var contents = new List<BuildableInfo>();
+            foreach (var layout in layouts)
+                for (var i = 0; i < sizes.z; ++i)
+                for (var j = 0; j < sizes.x; ++j)
+                    if (layout[i, j] != null)
+                    {
+                        var symbolDef = layout[i, j];
+                        var traverse = Traverse.Create(symbolDef);
+                        var thingDef = traverse.Field("thingDef").GetValue<ThingDef>();
+                        var stuffDef = traverse.Field("stuffDef").GetValue<ThingDef>();
+                        var rotation = traverse.Field("rotation").GetValue<Rot4>();
+                        var position = new IntVec3(j, 0, i);
+                        var thing = CreateThing(thingDef, stuffDef, position, rotation);
+                        if (thingDef == null)
+                            Log.Message($"{symbolDef} not found, set to null");
+                        else if (!thingDef.BuildableByPlayer)
+                            Log.Message($"{thingDef} is not buildable by plater, set to null");
+                        else
+                            contents.Add(new BuildableInfo(thing, IntVec3.Zero));
+                    }
+
+            var terrainGrid = Traverse
+                .Create(structureLayoutDef)
+                .Field("_terrainGrid")
+                .GetValue<TerrainDef[,]>();
+            for (var i = 0; i < sizes.z; ++i)
+            for (var j = 0; j < sizes.x; ++j)
+                if (terrainGrid[i, j] != null)
+                {
+                    var terrain = terrainGrid[i, j];
+                    if (DefDatabase<TerrainDef>.GetNamed(terrain.defName) == null)
+                        Log.Message($"{terrain.defName} not found, set to null");
+                    else if (!terrain.BuildableByPlayer)
+                        Log.Message($"{terrain.defName} is not buildable by player, set to null");
+                    else
+                        contents.Add(
+                            new BuildableInfo(terrain, new IntVec3(j, 0, i), IntVec3.Zero)
+                        );
+                }
+            return new Blueprint(contents, sizes, name);
+        }
+
+        public static Thing CreateThing(
+            ThingDef thingDef,
+            ThingDef stuff,
+            IntVec3 position,
+            Rot4 rotation
+        )
+        {
+            var thing = new Thing { def = thingDef };
+            var traverse = Traverse.Create(thing);
+            traverse.Field("stuffInt").SetValue(stuff);
+            traverse.Field("positionInt").SetValue(position);
+            traverse.Field("rotationInt").SetValue(rotation);
+            return thing;
+        }
+    }
+}
